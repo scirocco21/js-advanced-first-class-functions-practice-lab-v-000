4b314c13d8e49b6daf7abae73cf91bf0a26ebb2f
Seb Gertz <sebgertz@gmail.com>
2018-05-11 16:47:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function logDriversByHometown(drivers, location) {
     }
   })
 }
+
+function driversByRevenue(drivers) {
+  drivers.sort(function(a, b) {
+    return a.revenue - b.revenue
+  })
+}
