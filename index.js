1d8928b5f4d125ec9e7009b3181552d1f42405fc
Seb Gertz <sebgertz@gmail.com>
2018-05-11 17:42:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,5 +35,10 @@ function totalRevenue(drivers) {
 }
 
 function averageRevenue(drivers) {
-  
+  const revenues = [];
+  drivers.forEach(function(driver){revenues.push(driver.revenue)})
+  return revenues.reduce(function(accumulator, currentValue, currentIndex, array) {
+    return accumulator + currentValue/drivers.length;
+  })
+}
 }
